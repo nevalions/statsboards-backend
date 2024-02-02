af21937f8188247fb711ec267e12a8c9660b7bc1
linroot <linroot@bk.ru>
2024-02-02 15:04:13 +0300

    added match front and backend

--- src/helpers/fetch_helpers.py
+++ src/helpers/fetch_helpers.py
@@ -64,3 +64,34 @@ async def fetch_match_data(match_id: int):
         return {
             "status_code": status.HTTP_404_NOT_FOUND,
         }
+
+
+async def fetch_with_scoreboard_data(match_id: int):
+    match_data_service_db = MatchDataServiceDB(db)
+    match_service_db = MatchServiceDB(db)
+
+    scoreboard_data = await match_service_db.get_scoreboard_by_match(match_id)
+    match = await match_service_db.get_by_id(match_id)
+    match_teams_data = await match_service_db.get_teams_by_match(match_id)
+    match_data = await match_service_db.get_matchdata_by_match(match_id)
+
+    if match:
+        if match_data is None:
+            match_data_schema = MatchDataSchemaCreate(match_id=match_id)
+            match_data = await match_data_service_db.create_match_data(
+                match_data_schema
+            )
+
+        return {
+            "match_id": match_id,
+            "id": match_id,
+            "status_code": status.HTTP_200_OK,
+            "match": match,
+            "scoreboard_data": scoreboard_data,
+            "teams_data": match_teams_data,
+            "match_data": match_data.__dict__,
+        }
+    else:
+        return {
+            "status_code": status.HTTP_404_NOT_FOUND,
+        }
