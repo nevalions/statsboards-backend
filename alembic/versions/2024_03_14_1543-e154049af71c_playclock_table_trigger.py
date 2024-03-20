a80073dd521f2f3e882ccf7fda8ef22e00560523
linroot <linroot@bk.ru>
2024-03-20 14:45:11 +0300

    image alembic removed e154049af71c

--- alembic/versions/2024_03_14_1543-e154049af71c_playclock_table_trigger.py
+++ alembic/versions/2024_03_14_1543-e154049af71c_playclock_table_trigger.py
@@ -19,35 +19,37 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade():
     # Create notify function and trigger for PlayClockDB
-    op.execute("""
-    CREATE OR REPLACE FUNCTION notify_playclock_change() RETURNS trigger AS $$
-    DECLARE
-    BEGIN
-        IF (TG_OP = 'DELETE') THEN
-            PERFORM pg_notify('playclock_change', json_build_object('table', TG_TABLE_NAME, 'operation', TG_OP, 'old_id', OLD.id)::text); 
-        ELSE
-            PERFORM pg_notify('playclock_change', json_build_object('table', TG_TABLE_NAME, 'operation', TG_OP, 'new_id', NEW.id, 'old_id', OLD.id)::text); 
-        END IF;
-        RETURN new;
-    END;
-    $$ LANGUAGE plpgsql;
-    """)
-
-    # Create a trigger on the PlayClockDB
-    op.execute("""
-    CREATE TRIGGER playclock_change
-    AFTER INSERT OR UPDATE OR DELETE ON playclock
-    FOR EACH ROW EXECUTE PROCEDURE notify_playclock_change();
-    """)
+    # op.execute("""
+    # CREATE OR REPLACE FUNCTION notify_playclock_change() RETURNS trigger AS $$
+    # DECLARE
+    # BEGIN
+    #     IF (TG_OP = 'DELETE') THEN
+    #         PERFORM pg_notify('playclock_change', json_build_object('table', TG_TABLE_NAME, 'operation', TG_OP, 'old_id', OLD.id)::text);
+    #     ELSE
+    #         PERFORM pg_notify('playclock_change', json_build_object('table', TG_TABLE_NAME, 'operation', TG_OP, 'new_id', NEW.id, 'old_id', OLD.id)::text);
+    #     END IF;
+    #     RETURN new;
+    # END;
+    # $$ LANGUAGE plpgsql;
+    # """)
+    #
+    # # Create a trigger on the PlayClockDB
+    # op.execute("""
+    # CREATE TRIGGER playclock_change
+    # AFTER INSERT OR UPDATE OR DELETE ON playclock
+    # FOR EACH ROW EXECUTE PROCEDURE notify_playclock_change();
+    # """)
+    pass
 
 
 def downgrade():
+    pass
     # Remove trigger (if created)
-    op.execute("""
-    DROP TRIGGER IF EXISTS playclock_change ON playclock CASCADE;
-    """)
-
-    # Drop notify function
-    op.execute("""
-    DROP FUNCTION IF EXISTS playclock_change() CASCADE;
-    """)
+    # op.execute("""
+    # DROP TRIGGER IF EXISTS playclock_change ON playclock CASCADE;
+    # """)
+    #
+    # # Drop notify function
+    # op.execute("""
+    # DROP FUNCTION IF EXISTS playclock_change() CASCADE;
+    # """)
