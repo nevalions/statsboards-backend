775a795daa8b267f7434c65bc33775af790fee57
nevalions <nevalions@gmail.com>
2024-08-05 18:06:38 +0300

    players in match eesl_id not unique

--- src/player_match/db_services.py
+++ src/player_match/db_services.py
@@ -72,17 +72,6 @@ class PlayerMatchServiceDB(BaseServiceDB):
                 detail=f"Player_match " f"id({p}) " f"returned some error",
             )
 
-    # async def update_player_match_by_eesl(
-    #     self,
-    #     eesl_field_name: str,
-    #     p: PlayerMatchSchemaUpdate,
-    # ):
-    #     return await self.update_item_by_eesl_id(
-    #         eesl_field_name,
-    #         p.player_match_eesl_id,
-    #         p,
-    #     )
-
     async def create_new_player_match(
         self,
         p: PlayerMatchSchemaCreate,
@@ -99,16 +88,6 @@ class PlayerMatchServiceDB(BaseServiceDB):
 
         # print('player_match', player_match)
         return await super().create(player_match)
-
-    # async def get_player_match_by_eesl_id(
-    #     self,
-    #     value,
-    #     field_name="player_match_eesl_id",
-    # ):
-    #     return await self.get_item_by_field_value(
-    #         value=value,
-    #         field_name=field_name,
-    #     )
 
     async def get_player_match_by_match_id_and_eesl_id(
         self, match_id, player_match_eesl_id
