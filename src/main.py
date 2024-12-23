6d85c16451f94cb32ebee2d71eb2d9fc38508a5d
linroot <nevalions@live.com>
2024-12-23 14:46:46 +0300

    adding logging to start server

--- src/main.py
+++ src/main.py
@@ -33,36 +33,9 @@ from src.teams import api_team_router
 from src.tournaments import api_tournament_router, template_tournament_router
 
 logger = logging.getLogger("backend_logger_fastapi")
-
-# print(f'Absolute parent path: {Path(__file__).parent.absolute()}')
-# logs_dir = Path(__file__).parent / "logs"
-# logs_config_yaml = Path(__file__).parent / "logging-config.yaml"
-# logs_dir.mkdir(parents=True, exist_ok=True)
-#
-# def setup_logging(config_path=logs_config_yaml):
-#     # Resolve the full path of the config file
-#     print(f'Loading logging configuration from {config_path}')
-#
-#     # Load YAML configuration
-#     with open(config_path, "r") as file:
-#         config = yaml.safe_load(file)
-#
-#     # Define the log directory and add it to the configuration
-#     logs_dir.mkdir(parents=True, exist_ok=True)
-#
-#     # Replace the placeholder with the actual log directory path
-#     config['handlers']['file']['filename'] = str(logs_dir / "backend.log")
-#
-#     # Apply the logging configuration
-#     logging.config.dictConfig(config)
-#
-#     print(f'Logging setup completed. Log file: {config["handlers"]["file"]["filename"]}')
-
 setup_logging()
-# Check if the log file is writable
+
 log_file_path = logs_dir / "backend.log"
-
-
 if os.access(log_file_path, os.W_OK):
     logger.debug("Log file is writable.")
 else:
@@ -98,10 +71,8 @@ app.add_event_handler("startup", ws_manager.startup)
 app.add_event_handler("shutdown", ws_manager.shutdown)
 
 logger.info("FastAPI app initialized.")
-# print("FASTAPI STATSBOARD STARTED!")
 allowed_origins = os.getenv("ALLOWED_ORIGINS", "*")
 origins = [allowed_origins] if allowed_origins == "*" else allowed_origins.split(",")
-# print(f"allowed_origins: {origins}")
 logger.info(f"allowed_origins: {origins}")
 
 app.add_middleware(
