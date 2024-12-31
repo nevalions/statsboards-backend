f86f2a7f511e291da095d8e671d9695fd30fc1ed
linroot <nevalions@live.com>
2024-12-31 21:28:51 +0300

    refactor TeamTournamentServiceDB

--- src/team_tournament/db_services.py
+++ src/team_tournament/db_services.py
@@ -17,25 +17,6 @@ class TeamTournamentServiceDB(BaseServiceDB):
         self.logger = get_logger("backend_logger_TeamTournamentServiceDB", self)
         self.logger.debug(f"Initialized TeamTournamentServiceDB")
 
-    # async def create_team_tournament_relation(
-    #         self,
-    #         tournament_id: int,
-    #         team_id: int,
-    #         tournament_id_name: str = "tournament_id",
-    #         team_id_name: str = "team_id",
-    #         child_relation="teams",
-    # ):
-    #     return await self.create_m2m_relation(
-    #         parent_model=TournamentDB,
-    #         child_model=TeamDB,
-    #         secondary_table=text("team_tournament"),
-    #         parent_id=tournament_id,
-    #         child_id=team_id,
-    #         parent_id_name=tournament_id_name,
-    #         child_id_name=team_id_name,
-    #         child_relation=child_relation,
-    #     )
-
     async def create_team_tournament_relation(
         self,
         team_tournament: TeamTournamentSchemaCreate,
@@ -118,22 +99,3 @@ class TeamTournamentServiceDB(BaseServiceDB):
                 status_code=400,
                 detail=f"Error on delete connection team id: {team_id} and tournament id {tournament_id}",
             )
-
-
-# async def get_team_tour_db() -> TeamTournamentServiceDB:
-#     yield TeamTournamentServiceDB(db)
-#
-#
-# async def async_main() -> None:
-#     team_service = TeamTournamentServiceDB(db)
-#     # dict_conv = TeamTournamentSchemaCreate(**{'fk_team': 8, 'fk_tournament': 3})
-#     # t = await team_service.create_team_tournament_relation(dict_conv)
-#     # t = await team_service.get_teams_by_tournament(3)
-#     # if t:
-#     #     print(t)
-#     # else:
-#     #     pass
-#
-#
-# if __name__ == "__main__":
-#     asyncio.run(async_main())
